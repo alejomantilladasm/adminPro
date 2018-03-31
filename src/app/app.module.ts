1584f73217662bc07b776b088ee5594aa9ac726b
Alejandro Mantilla <alejomantillasam@hotmail.com>
2018-03-31 12:34:57 -0500

    modificacion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,38 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // Rutas
-import { AppRroutes } from './app.routes';
-
+import { APP_ROUTES } from './app.routes';
+// Modulos
+import { PagesModule } from './pages/pages.module';
+// Componets
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashBoardComponent } from './pages/dash-board/dash-board.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SideBarComponent } from './shared/side-bar/side-bar.component';
-import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
-import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NoPageFoundComponent,
-    DashBoardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SideBarComponent,
-    BreadCrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRroutes
+    PagesModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
